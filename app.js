9e5621aa5aa600a47a460f144b0629e963d8df79
raidenot <23019328@myrp.edu.sg>
2024-08-01 11:10:11 +0800

    Second Commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const mysql = require('mysql2');
+const multer = require('multer');
+const app = express();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({storage: storage});
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:true}));
+
+const connection = mysql.createConnection({
+host: 'localhost',
+user: 'root',
+password: '',
+database: 'c237_supermarketapp'
+});
+
+connection.connect((err) => {
+if (err) {
+console.error('Error connecting to MySQL:', err);
+return;
+}
+
+console.log('Connected to MySQL database');
+});
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
